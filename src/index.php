2786430c87234976e39cb339421b3ec84d63b834
Alfredo Scaini <alfredo.scaini@cse-cst.gc.ca>
2023-12-01 16:45:26 -0500

    containerized

---
+++ src/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Advent Calendar 2023</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
+</head>
+
+<body>
+
+  <div class="container-fluid">    
+    <div class="row content">
+      <div class="col-sm-2 sidenav"></div>
+
+      <div class="col-sm-8"> 
+        <h1>Advent Calendar Code</h1>
+        <ul>
+          <li><a href="01-dec/">December 1, 2023</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+ 
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
