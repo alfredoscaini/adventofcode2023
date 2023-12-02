803b30015242c251f5e9606ee01d10b6063dbc28
FredLabs <git>
2023-12-02 07:32:32 -0500

    Having too much fun

--- src/index.php
+++ src/index.php
@@ -1,35 +1,49 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Advent Calendar 2023</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <link href="style.css" rel="stylesheet" />
 </head>
 
-<body>
+<body class="d-flex flex-column h-100">
 
-  <div class="container-fluid">    
-    <div class="row content">
-      <div class="col-sm-2 sidenav"></div>
+  <main class="flex-shrink-0">
+    <div class="container">
+      <h1 class="mt-5">Advent Calendar 2023</h1>
 
-      <div class="col-sm-8"> 
-        <h1>Advent Calendar Code</h1>
-        <ul>
-          <li><a href="01-dec/">December 1, 2023</a></li>
-        </ul>
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title">Day 2: Cube Conundrum</h2>
+          <p class="card-text">To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.</p>
+          <a href="01-dec/" class="btn btn-primary">02 December results</a>
+        </div>
       </div>
+      
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title">Day 1: Trebuchet?!</h2>
+          <p class="card-text">The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover.</p>
+          <a href="01-dec/" class="btn btn-primary">01 December results</a>
+        </div>
+      </div>
+      
     </div>
-  </div>
- 
+  </main>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container text-center">
+      <span class="text-muted">ASC2C</span>
+    </div>
+  </footer>
+
+   
 </body>
 </html>
