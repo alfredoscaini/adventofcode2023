29dc35b45ebb399efec30d03312c5bcd8b040943
Alfredo Scaini <alfredo.scaini@cse-cst.gc.ca>
2023-12-05 18:25:43 -0500

    So close!

--- src/05-dec/index.php
+++ src/05-dec/index.php
@@ -19,6 +19,28 @@ class Map {
   }
 }
 
+class MapRange {
+  use Extract;
+
+  private $id;
+  public $seedRange;
+  public $soilRange;
+  public $fertilizerRange;
+  public $waterRange;
+  public $lightRange;
+  public $temperatureRange;
+  public $humidityRange;
+  public $locationRange;
+
+  public function __construct($id = 0, $min = 0, $max = 0) {
+    $this->id   = $id;
+    $this->seedRange = [
+      'min' => $min, 
+      'max' => $max
+    ];
+  }
+}
+
 trait Extract {
   public function getID($id = 0, $data = []) {
     $result = $id;
@@ -39,38 +61,46 @@ trait Extract {
 
     return $result;
   }
-}
 
+  function getRange($range = [], $data = []) {
 
-function findLowest($location_min, $lowest, $range, $seed_to_soil, $soil_to_fertilizer, $fertilizer_to_water, $water_to_light, $light_to_temperature, $temperature_to_humidity, $humidity_to_location) {
+    $source_min = $range['min'];
+    $source_max = $range['max'];
   
-  for ($seed = $lowest; $seed <= $range; $seed++) {
-    $id  = 0;
-    $map = new Map($id, $seed);
+    $result_min = $source_min;
+    $result_max = $source_max;
+  
+    foreach ($data as $item) {
+      $destination_range_start = $item[0];
+      $source_range_start      = $item[1];        
+      $range_legnth            = $item[2];
+  
+      $source_range_end       = $source_range_start + $range_legnth;
+      $destination_range_end  = $destination_range_start + $range_legnth;
 
-    $map->soil        = $map->getID($map->seed, $seed_to_soil);
-    $map->fertilizer  = $map->getID($map->soil, $soil_to_fertilizer);
-    $map->water       = $map->getID($map->fertilizer, $fertilizer_to_water);
-    $map->light       = $map->getID($map->water, $water_to_light);
-    $map->temperature = $map->getID($map->light, $light_to_temperature);
-    $map->humidity    = $map->getID($map->temperature, $temperature_to_humidity);
-    $map->location    = $map->getID($map->humidity, $humidity_to_location);
+      if (($source_min > $source_range_start) ) {
+        $increment = $source_min - $source_range_start;
+        $result_min = $destination_range_start + $increment;
+      }
 
-    if (is_null($location_min)) {
-      $location_min = $map->location;
+      if (($source_max < $source_range_end)) {
+        $increment = $source_max - $source_range_end;
+        $result_max = $destination_range_end + $increment;
+      }
     }
-
-    if ($location_min > $map->location) {
-      $location_min = $map->location;
-    }
-
-    yield $location_min;
+  
+    return [
+      'min' => $result_min, 
+      'max' => $result_max
+    ];
   }
 }
+
 
 // ---------------------------------------------------------
 // Main
 // ---------------------------------------------------------
+date_default_timezone_set("America/New_York");
 require_once('data.php');
 
 $maps = [];
@@ -102,33 +132,40 @@ foreach ($maps as $map) {
 print '<p>The lowest location value is ' . $location_min . '</p>';
 
 // Question 2: What is the lowest location value from a range of seeds?
+print "<p>Start time is " . date("h:i:sa") . '</p>';
 
-$location_min = null;
+
+$maps         = [];
 foreach ($seed_ranges as $key => $item) {
-  $seed_start  = $item[0];
-  $range       = $seed_start + $item[1];
-
-  for ($seed = $seed_start; $seed <= $range; $seed++) {
-    $id  = 0;
-    $map = new Map($id, $seed);
-
-    $map->soil        = $map->getID($map->seed, $seed_to_soil);
-    $map->fertilizer  = $map->getID($map->soil, $soil_to_fertilizer);
-    $map->water       = $map->getID($map->fertilizer, $fertilizer_to_water);
-    $map->light       = $map->getID($map->water, $water_to_light);
-    $map->temperature = $map->getID($map->light, $light_to_temperature);
-    $map->humidity    = $map->getID($map->temperature, $temperature_to_humidity);
-    $map->location    = $map->getID($map->humidity, $humidity_to_location);    
-  }
-
-  if (is_null($location_min)) {
-    $location_min = $map->location;
-  }
-
-  if ($location_min > $map->location) {
-    $location_min = $map->location;
-  }
-
+  $min       = $item[0];
+  $max       = $min + $item[1];
+  $maps[]     = new MapRange($key, $min, $max);
 }
 
-print '<p>The lowest location value is ' . $location_min . '</p>';
+$location_min = null;
+foreach ($maps as $map) {
+  $map->soilRange        = $map->getRange($map->seedRange, $seed_to_soil);
+  $map->fertilizerRange  = $map->getRange($map->soilRange, $soil_to_fertilizer);
+  $map->waterRange       = $map->getRange($map->fertilizerRange, $fertilizer_to_water);
+  $map->lightRange       = $map->getRange($map->waterRange, $water_to_light);
+  $map->temperatureRange = $map->getRange($map->lightRange, $light_to_temperature);
+  $map->humidityRange    = $map->getRange($map->temperatureRange, $temperature_to_humidity);
+  $map->locationRange    = $map->getRange($map->humidityRange, $humidity_to_location);
+
+  if (is_null($location_min)) {
+    $location_min = $map->locationRange['min'];
+  }
+
+  if ($location_min > $map->locationRange['min']) {
+    $location_min = $map->locationRange['min'];
+  }
+}
+
+
+print '<p>The minimum location is ' . $location_min . '</p>';
+print "<p>End time is " . date("h:i:sa") . '</p>';
+
+print '<pre>';
+print_r($maps);
+
+exit;
